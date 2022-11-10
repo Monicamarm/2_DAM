d398a08fbae4b59ee44ced6a29eeda577140def9
Monicamarm <monica.s269606@cesurformacion.com>
2022-11-10 20:24:45 +0100

    app

--- Programacion_android/PrimerProyecto/App.js
+++ Programacion_android/PrimerProyecto/App.js
@@ -7,87 +7,117 @@ export default function App() {
     const [correo, setCorreo] = useState(null);
     const [sexo, setSexo] = useState(null);
     const [text, setText] = useState(null);
-    
-    /*function Texto(edad, correo) {
-        const regex=/^[a-zA-Z]+$/;
-        const reg =/^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
-        
-        if (!edad.match(regex) && !correo.match(reg)) {
-            <View>
-            <Text>Mi nombre es {nombre} con edad {edad} , y correo {correo}</Text>
-            <Text> Sexo {sexo}</Text>
-        </View>
-          } else {
-            
-            setText('Eso no vale listillo')
-            setColor('yellow')
-          }
-    }*/
+    const [isEnabled, setIsEnabled] = useState(false)
+
+
+    function Texto(edad, correo) {
+        const regex = /^[a-zA-Z]+$/;
+        const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+
+        if (!edad.match(regex) && !correo.text === true && Object.keys(edad).length != 0 && Object.keys(correo).length != 0) {
+            setText('Mi nombre es ' + nombre + ' con edad ' + edad + ' , y correo ' + correo + ' Sexo ' + sexo);
+
+        } else {
+            setText('Edad o correo invalidos')
+        }
+
+    }
 
     return (
         <View>
             <View style={{ flexDirection: 'row' }}>
-                <Text>Nombre</Text>
+                <Text>Nombre: </Text>
                 <TextInput
                     style={{
+                        marginLeft:25,
                         padding: 0,
                         height: 30,
                         width: 275,
                         borderColor: 'grey',
                         borderWidth: 1
                     }}
-                    placeholder="Nombre"
+                    placeholder=" Nombre"
                     onChangeText={nombre => setNombre(nombre)}
                 />
             </View>
             <View style={{ flexDirection: 'row' }}>
-                <Text>Apellidos</Text>
+                <Text>Apellidos: </Text>
                 <TextInput
                     style={{
+                        marginLeft:16,
                         padding: 0,
                         height: 30,
                         width: 275,
                         borderColor: 'grey',
                         borderWidth: 1
                     }}
-                    placeholder="Apellidos"
+                    placeholder=" Apellidos"
                     onChangeText={apellidos => setApellidos(apellidos)}
                 />
             </View>
             <View style={{ flexDirection: 'row' }}>
-                <Text>Edad</Text>
+                <Text>Edad: </Text>
                 <TextInput
                     style={{
+                        marginLeft:48,
                         padding: 0,
                         height: 30,
                         width: 275,
                         borderColor: 'grey',
                         borderWidth: 1
                     }}
-                    placeholder="Edad"
+                    placeholder=" Edad"
                     onChangeText={edad => setEdad(edad)}
                 />
             </View>
             <View style={{ flexDirection: 'row' }}>
-                <Text>Correo</Text>
+                <Text>Correo: </Text>
                 <TextInput
                     style={{
+                        marginLeft:35,
                         padding: 0,
                         height: 30,
                         width: 275,
                         borderColor: 'grey',
                         borderWidth: 1
                     }}
-                    placeholder="Correo"
+                    placeholder=" Correo"
                     onChangeText={correo => setCorreo(correo)}
                 />
             </View>
-            
+            <View style={{ flexDirection: 'row' }}>
+                <Text>Sexo: </Text>
+                <Switch
+                    trackColor={{ false: 'aqua', true: 'pink' }}
+                    thumbColor={isEnabled ? 'aqua' : 'pink'}
+                    onValueChange={() => setIsEnabled(previousState => !previousState)}
+                    value={isEnabled}
+                />
+                {isEnabled ? <Text style={{ alignItems: 'center' }}>  Masculino</Text> : <Text style={{ alignItems: 'center' }}>  Femenino</Text>}
+           </View>
             <View style={{ padding: 55 }}>
                 <Button title='Enviar'
-                    onPress={}
-                />
+                    onPress={() => { Texto(edad, correo) }}
+                    />
+            </View>
+            <Text style={{ alignItems: 'center', borderRadius:15 }}>{text}</Text>
+            <View>
+                < Image
+                    style={{
+                        height: 350,
+                        width: 450,
+                        alignItems: 'center'
+                    }}
+                    source={{
+                        uri: 'https://i.pinimg.com/600x315/2f/57/b5/2f57b5ef82d8024b39beec08a15f5a0d.jpg'
+                    }}
+                    />
             </View>
         </View>
     )
 }
+// if (isEnabled) {
+//     setSexo=('Masculino')
+// }else{
+//     setSexo=('Femenino')
+// }
