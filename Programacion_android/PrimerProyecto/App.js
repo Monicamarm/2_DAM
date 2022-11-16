c1c24c788d2960837d124a95369b0938b6f17e8c
Monicamarm <99404491+Monicamarm@users.noreply.github.com>
2022-11-16 14:43:59 +0100

    app

--- Programacion_android/PrimerProyecto/App.js
+++ Programacion_android/PrimerProyecto/App.js
@@ -1,143 +1,125 @@
-import React, { useState } from 'react';
-import { StyleSheet, Button, Text, TextInput, ScrollView,View, Image, Switch} from 'react-native';
-import { Boton } from './Boton';
+import React, { Component } from 'react';
+import { View, Text, FlatList } from 'react-native';
 
 export default function App() {
-    const [edad, setEdad] = useState('');
-    const [nombre, setNombre] = useState('null');
-    const [apellidos, setApellidos] = useState('null');
-    const [correo, setCorreo] = useState('null');
-    const [text, setText] = useState(' ');
-    const [isEnabled, setIsEnabled] = useState(false)
-    const [escondido, setEscondido] = useState(false)
-
-
-    function Texto(edad, correo) {
-        const regex = /^[0-9]*$/;
-        const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
-        const regN = /^[a-zA-ZÀ-ÿ\s]{1,40}$/;
-
-        if (edad.match(regex) && correo.match(reg) && !edad == '' && nombre.match(regN) && apellidos.match(regN)) {
-            setText('Mi nombre es ' + nombre + ' con edad ' + edad + ' , y correo ' + correo + ' Sexo ' + (isEnabled ? 'Masculino' : 'Femenino'));
-            setEscondido(true);
-        } else {
-            setText(' ')
-            alert('Campos invalidos')
-        }
+  const DATA = [
+    {
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      title: 'Primer elemento',
+      nombre: 'Ana',
+      edad: '2000'
+    },
+    {
+      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      title: 'Segundo elemento',
+      nombre: 'Bernat',
+      edad: '2001'
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Tercero elemento',
+      nombre: 'Carmen',
+      edad: '2002'
+    }, {
+      id: '58694a0f-3da1-471f-bd96-145571e29d32',
+      title: 'Cuarto elemento',
+      nombre: 'Daniel',
+      edad: '2003'
+    }, {
+      id: '58694a0f-3da1-471f-bd96-145571e29d27',
+      title: 'Quinto elemento',
+      nombre: 'Estefania',
+      edad: '2004'
+    }, {
+      id: '58694a0f-3da1-471f-bd96-145571e29d13',
+      title: 'Sexto elemento',
+      nombre: 'Fernando',
+      edad: '2005'
+    }, {
+      id: '58694a0f-3da1-471f-bd96-145571e29d702',
+      title: 'Septimo elemento',
+      nombre: 'Grace',
+      edad: '2006'
+    }, {
+      id: '58694a0f-3da1-471f-bd96-145571e29d207',
+      title: 'Octavo elemento',
+      nombre: 'hugo',
+      edad: '2007'
+    }, {
+      id: '58694a0f-3da1-471f-bd96-145571e29d720',
+      title: 'Noveno elemento',
+      nombre: 'Imma',
+      edad: '2008'
+    }, {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Décimo elemento',
+      nombre: '',
+      edad: ''
+    }, {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Undecimo elemento',
+      nombre: '',
+      edad: ''
+    }, {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Duodecimo elemento',
+      nombre: '',
+      edad: ''
+    }, {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Decimotercero elemento',
+      nombre: '',
+      edad: ''
+    }, {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Decimocuarto elemento',
+      nombre: '',
+      edad: ''
+    }, {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Decimoquinto elemento',
+      nombre: '',
+      edad: ''
+    }, {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Decimosexto elemento',
+      nombre: '',
+      edad: ''
+    }, {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Decimoseptimo elemento',
+      nombre: '',
+      edad: ''
+    }, {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Decimooctavo elemento',
+      nombre: '',
+      edad: ''
+    }, {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Decimonoveno elemento',
+      nombre: '',
+      edad: ''
+    }, {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Bigesimo elemento',
+      nombre: '',
+      edad: ''
     }
-
-    return (
-        <ScrollView style={{ flex: 1, backgroundColor: '#e0b0ff' }}>
-            <Text style={{ color: 'white', textAlign: 'center', fontSize: 50, marginBottom:35 }}>Formulario</Text>
-            <View style={{ flexDirection: 'row' }}>
-                <Text style={{ color: 'white', fontSize: 18,marginBottom:15 }}>Nombre: </Text>
-                <TextInput
-                    style={{
-                        marginLeft: 25,
-                        padding: 0,
-                        height: 32,
-                        width: 275,
-                        borderColor: '#d3d3d3',
-                        borderWidth: 1,
-                        fontSize: 18
-
-                    }}
-                    placeholder=" Nombre"
-                    onChangeText={nombre => setNombre(nombre)}
-                />
-            </View>
-            <View style={{ flexDirection: 'row' }}>
-                <Text style={{ color: 'white', fontSize: 18,marginBottom:15 }}>Apellidos: </Text>
-                <TextInput
-                    style={{
-                        marginLeft: 13.5,
-                        padding: 0,
-                        height: 32,
-                        width: 275,
-                        borderColor: '#d3d3d3',
-                        borderWidth: 1,
-                        fontSize: 18
-                    }}
-                    placeholder=" Apellidos"
-                    onChangeText={apellidos => setApellidos(apellidos)}
-                />
-            </View>
-            <View style={{ flexDirection: 'row' }}>
-
-                <Text style={{ color: 'white', fontSize: 18,marginBottom:15 }}>Edad: </Text>
-                <TextInput
-                    style={{
-                        marginLeft: 54.4,
-                        padding: 0,
-                        height: 32,
-                        width: 275,
-                        borderColor: '#d3d3d3',
-                        borderWidth: 1,
-                        fontSize: 18
-                    }}
-                    placeholder=" Edad"
-                    onChangeText={edad => setEdad(edad)}
-                />
-            </View>
-            <View style={{ flexDirection: 'row' }}>
-                <Text style={{ color: 'white', fontSize: 18 ,marginBottom:15}}>Correo: </Text>
-                <TextInput
-                    style={{
-                        marginLeft: 37,
-                        padding: 0,
-                        height: 32,
-                        width: 275,
-                        borderColor: '#d3d3d3',
-                        borderWidth: 1,
-                        fontSize: 18
-                    }}
-                    placeholder=" Correo"
-                    onChangeText={correo => setCorreo(correo)}
-                />
-            </View>
-            <View style={{ flexDirection: 'row' }}>
-                <Text style={{
-                    marginTop: 5,
-                    fontSize: 18,
-                    color: 'white'
-                }}>Sexo: </Text>
-                <Switch style={{
-                    marginLeft: 45,
-                    marginTop: 5,
-                }}
-                    trackColor={{ false: 'aqua', true: 'pink' }}
-                    thumbColor={isEnabled ? 'aqua' : 'pink'}
-                    onValueChange={() => setIsEnabled(previousState => !previousState)}
-                    value={isEnabled}
-                />
-                {isEnabled ? <Text style={{ alignItems: 'center', marginTop: 5, color: 'white', fontSize: 18 }}>  Masculino</Text> : <Text style={{ alignItems: 'center', marginTop: 5, color: 'white', fontSize: 18 }}>  Femenino</Text>}
-            </View>
-            <View >
-
-            </View>
-            <Boton onPress={() => { Texto(edad, correo) }} />
-            <View>
-
-                <Text style={{ alignItems: 'center' }}>{text}</Text>
-                {
-                    !escondido
-                        ? null
-                          :
-                          (isEnabled?<Image style={{
-                            height: 434,
-                            width: 415
-                            }}
-                            source={ require('./monop.jpg') }
-                          />:<Image style={{
-                            height: 450,
-                            width: 415
-                            }}
-                            source={require('./mono.jpg')} />)
-                        }
-            </View>
-
-
-        </ScrollView>
-    )
+  ];
+  const renderItem = ({ item }) => (
+    <View>
+      <Text>{item.title}</Text>
+    </View>
+  )
+  return (
+    <View>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+    </View>
+  )
 
 }
